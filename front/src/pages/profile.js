cec9aa0e02b3928420c4d7808af69962ee748832
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-12-05 21:30:54 +0900

    feat : add profile page component

--- front/src/pages/profile.js
+++ front/src/pages/profile.js
@@ -2,17 +2,26 @@ import React from 'react';
 import Head from "next/head";
 
 import AppLayout from "../layout/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
 
 const Profile = () => {
-	return (
-		<>
-			<Head>
-	      <meta charSet="utf-8" />
-	      <title>내 프로필 | NodeBird</title>
+  const followerList = [{nickname: '넥스트'}, {nickname: '재밋네'}, {nickname: '고수가되고싶다'},];
+  const followingList = [{nickname: '넥스트'}, {nickname: '재밋네'}, {nickname: '고수가되고싶다'},];
+
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>내 프로필 | NodeBird</title>
       </Head>
-			<AppLayout>Hello, Profile</AppLayout>
-		</>
-	)
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList}/>
+        <FollowList header="팔로워 목록" data={followerList}/>
+      </AppLayout>
+    </>
+  )
 }
 
 export default Profile;
