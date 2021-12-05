a445662d55cba85b1f18903398db86c18c6d0cdf
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-12-05 20:44:11 +0900

    feat : antd 디자인 적용 및 더미데이터로 로그인 인터렉션 추가

--- front/src/layout/AppLayout.js
+++ front/src/layout/AppLayout.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import styled from "styled-components";
+import {Menu, Input, Row, Col} from 'antd';
 
-const AppLayout = ({ children }) => {
-	return (
-		<div>
-			<div>
-				<Link href="/"><a>노드버드</a></Link>
-				<Link href="/profile"><a>노드버드</a></Link>
-				<Link href="/signup"><a>노드버드</a></Link>
-			</div>
-			{children}
-		</div>
-	)
+import UserProfile from '../components/UserProfile';
+import LoginForm from '../components/LoginForm';
+
+const AppLayout = ({children}) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <div>
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href="/"><a>노드버드</a></Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/profile"><a>프로필</a></Link>
+        </Menu.Item>
+        <Menu.Item>
+          <SearchInput enterButton/>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup"><a>회원가입</a></Link>
+        </Menu.Item>
+      </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? <UserProfile setIsLoggedIn={setIsLoggedIn}/> : <LoginForm setIsLoggedIn={setIsLoggedIn}/>}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a href="https://www.github.com/jtwjs" target="_blank" rel="noreferrer noopener">Made by jtwjs</a>
+        </Col>
+      </Row>
+    </div>
+  )
 };
 
 AppLayout.propTypes = {
@@ -20,3 +45,7 @@ AppLayout.propTypes = {
 }
 
 export default AppLayout;
+
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`;
