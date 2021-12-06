47c716916e7a41bfe3190bc12e193b070037bbd2
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-12-06 23:58:16 +0900

    feat : redux 연동 작업 및 스토어 구축

---
+++ front/src/pages/_app.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+
+import 'antd/dist/antd.css';
+import wrapper from '../store/configureStore';
+
+const NodeBird = ({Component, pageProps}) => {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8"/>
+        <title>NodeBird</title>
+      </Head>
+      <Component {...pageProps}/>
+    </>
+  );
+};
+
+// prop을 점검해주면 서비스에 안정성이 높아짐
+NodeBird.propTypes =
+{
+  Component: PropTypes.elementType.isRequired,
+}
+
+export default wrapper.withRedux(NodeBird);
