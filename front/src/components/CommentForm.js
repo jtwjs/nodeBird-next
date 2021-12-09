e17997e2ac85a82c0706c3fb74ce8a0decc70153
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-12-09 21:36:00 +0900

    feat : 댓글 구현

---
+++ front/src/components/CommentForm.js
@@ -0,0 +1,28 @@
+import React, {useCallback} from 'react';
+import {useSelector} from 'react-redux';
+import PropTypes from 'prop-types';
+import { Form, Input, Button } from 'antd';
+
+import useInput from '../hooks/useInput';
+
+const CommentForm = ({post}) => {
+	const id = useSelector((state) => state.user.me?.id);
+	const [commentText, onChangeCommentText] = useInput('');
+	const onSubmitComment = useCallback(() => {
+
+	}, [commentText])
+	return (
+		<Form onFinish={onSubmitComment}>
+		  <Form.Item style={{ position: 'relative', margin: 0}}>
+			  <Input.TextArea value={commentText} onChange={onChangeCommentText} rows={4}/>
+			  <Button style={{ position: 'absolute', right: 0, bottom: -40}} type="primary" htmlType="submit">삐약</Button>
+		  </Form.Item>
+	  </Form>
+	)
+}
+
+CommentForm.propTypes = {
+	post: PropTypes.object.isRequired,
+}
+
+export default CommentForm;
--- front/src/components/PostCard.js
+++ front/src/components/PostCard.js
@@ -1,10 +1,11 @@
 import React, {useState, useCallback} from 'react';
 import {useSelector} from "react-redux";
 import PropTypes from 'prop-types';
-import {Card, Popover, Button, Avatar} from 'antd';
+import {Card, Popover, Button, Avatar, List, Comment} from 'antd';
 import {RetweetOutlined, HeartOutlined, MessageOutlined, EllipsisOutlined, HeartTwoTone} from '@ant-design/icons';
 
 import PostImages from "./PostImages";
+import CommentForm from "../components/CommentForm";
 
 const PostCard = ({post}) => {
 	const [liked, setLiked] = useState(false);
@@ -54,10 +55,23 @@ const PostCard = ({post}) => {
 			</Card>
 			{commentFormOpened && (
 				<div>
-					댓글 부분
+					<CommentForm post={post} />
+					<List
+						header={`${post.Comments.length}개의 댓글`}
+						itemLayout="horizontal"
+						dataSource={post.Comments}
+						renderItem={(item) => (
+							<li>
+								<Comment
+									author={item.User.nickname}
+									avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
+									content={item.content}
+								/>
+							</li>
+						)}
+					/>
 				</div>
 			)}
-			{/*<CommentForm/>*/}
 			{/*<Comments/>*/}
 		</div>
 	)
