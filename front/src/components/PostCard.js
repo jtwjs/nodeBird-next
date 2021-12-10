8a55d40196c27098e445450ec15a27db0cccc789
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-12-10 22:18:04 +0900

    feat : PostCardContent 컴포넌트 구현 및 해시태그 더미 추가

--- front/src/components/PostCard.js
+++ front/src/components/PostCard.js
@@ -6,6 +6,7 @@ import {RetweetOutlined, HeartOutlined, MessageOutlined, EllipsisOutlined, Heart
 
 import PostImages from "./PostImages";
 import CommentForm from "../components/CommentForm";
+import PostCardContent from "./PostCardContent";
 
 const PostCard = ({post}) => {
 	const [liked, setLiked] = useState(false);
@@ -50,7 +51,7 @@ const PostCard = ({post}) => {
 				<Card.Meta
 					avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
 					title={post.User.nickname}
-					description={post.content}
+					description={<PostCardContent postData={post.content} />}
 				/>
 			</Card>
 			{commentFormOpened && (
