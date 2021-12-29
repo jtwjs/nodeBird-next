ecd4cc460b1d196144f9ff0d0fef68a2a1c8f9f8
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-12-29 22:22:49 +0900

    Feat: 게시글 생성과 댓글 생성 api, saga 연결

--- front/src/sagas/post.js
+++ front/src/sagas/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import shortId from 'shortid';
-import {all, delay, fork, put, takeLatest, throttle} from 'redux-saga/effects';
+import {all, delay, fork, put, call, takeLatest, throttle} from 'redux-saga/effects';
 
 import {
   ADD_COMMENT_FAILURE,
@@ -41,24 +41,23 @@ function* loadPosts(action) {
 }
 
 function addPostAPI(data) {
-  return axios.post('/api/post', data);
+  return axios.post('/post', {content: data});
 }
 
 function* addPost(action) {
   try {
-    // const result = yield call(addPostAPI, action.data);
-    yield delay(1000);
+    const result = yield call(addPostAPI, action.data);
     const id = shortId.generate();
     yield put({
       type: ADD_POST_SUCCESS,
       data: {
         id,
-        content: action.data,
+        content: result.data,
       },
     });
     yield put({
 	    type: ADD_POST_TO_ME,
-	    data: id,
+	    data: result.data.id,
     })
   } catch (err) {
     console.error(err);
@@ -95,16 +94,15 @@ function* removePost(action) {
 }
 
 function addCommentAPI(data) {
-  return axios.post(`/api/post/${data.postId}/comment`, data);
+  return axios.post(`/post/${data.postId}/comment`, data);
 }
 
 function* addComment(action) {
   try {
-    // const result = yield call(addCommentAPI, action.data);
-    yield delay(1000);
+    const result = yield call(addCommentAPI, action.data);
     yield put({
       type: ADD_COMMENT_SUCCESS,
-      data: action.data,
+      data: result.data,
     });
   } catch (err) {
     yield put({
