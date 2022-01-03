5863f303934f7b0b5a02331220ea3982e13206d9
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2022-01-03 22:01:54 +0900

    Feat: 본인 게시글에 팔로우 버튼 안보이게 수정

--- front/src/components/FollowButton.js
+++ front/src/components/FollowButton.js
@@ -6,10 +6,10 @@ import {FOLLOW_REQUEST, UNFOLLOW_REQUEST} from "../reducers/user";
 
 const FollowButton = ({post}) => {
 	const dispatch = useDispatch();
-	const { me, followLoading, unfollowLoading } = useSelector((state) => state.user);
+	const {me, followLoading, unfollowLoading} = useSelector((state) => state.user);
 	const isFollowing = me && me.Followings.find(v => v.id === post.User.id);
 	const onClickButton = useCallback(() => {
-		if(isFollowing) {
+		if (isFollowing) {
 			dispatch({
 				type: UNFOLLOW_REQUEST,
 				data: post.User.id,
@@ -22,11 +22,15 @@ const FollowButton = ({post}) => {
 		}
 	}, [isFollowing]);
 
-  return (
-  	<Button loading={followLoading || unfollowLoading} onClick={onClickButton}>
-		  {isFollowing ? '언팔로우' : '팔로우'}
-  	</Button>
-  )
+	if (post.User.id === me.id) {
+		return null;
+	}
+
+	return (
+		<Button loading={followLoading || unfollowLoading} onClick={onClickButton}>
+			{isFollowing ? '언팔로우' : '팔로우'}
+		</Button>
+	)
 }
 
 FollowButton.propTypes = {
