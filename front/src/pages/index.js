6141c06b23328892418d96fe90a6226a9e1d0b59
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2022-01-10 22:49:40 +0900

    Feat: next.js life cycle과 redux 연결

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -1,61 +1,68 @@
 import React, {useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
-
+import {END} from 'redux-saga';
 import PostForm from '../components/PostForm';
 import PostCard from '../components/PostCard';
 import AppLayout from '../Layout/AppLayout';
+import wrapper from '../store/configureStore';
 import {LOAD_POSTS_REQUEST} from "../reducers/post";
 import {LOAD_MY_INFO_REQUEST} from '../reducers/user';
 
 const Home = () => {
-	const dispatch = useDispatch();
-	const {me} = useSelector(state => state.user);
-	const {mainPosts, hasMorePosts, loadPostsLoading, retweetError} = useSelector(state => state.post);
+  const dispatch = useDispatch();
+  const {me} = useSelector(state => state.user);
+  const {mainPosts, hasMorePosts, loadPostsLoading, retweetError} = useSelector(state => state.post);
 
-	useEffect(() => {
-		dispatch({
-			type: LOAD_MY_INFO_REQUEST,
-		})
-		dispatch({
-			type: LOAD_POSTS_REQUEST
-		})
-	}, []);
+  useEffect(() => {
+    function onScroll() {
+      if (Math.round(window.scrollY + document.documentElement.clientHeight) > document.documentElement.scrollHeight - 300) {
+        if (hasMorePosts && !loadPostsLoading) {
+          const lastId = mainPosts[mainPosts.length - 1]?.id;
+          dispatch({
+            type: LOAD_POSTS_REQUEST,
+            lastId,
+          })
+        }
+      }
+    }
 
-	useEffect(() => {
-		function onScroll() {
-			if (Math.round(window.scrollY + document.documentElement.clientHeight) > document.documentElement.scrollHeight - 300) {
-				if (hasMorePosts && !loadPostsLoading) {
-					const lastId = mainPosts[mainPosts.length - 1]?.id;
-				dispatch({
-					type: LOAD_POSTS_REQUEST,
-					lastId,
-				})
-				}
-			}
-		}
-		window.addEventListener('scroll', onScroll);
+    window.addEventListener('scroll', onScroll);
 
-		return () => {
-			window.removeEventListener('scroll', onScroll);
-		}
-	}, [hasMorePosts, loadPostsLoading, mainPosts]);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    }
+  }, [hasMorePosts, loadPostsLoading, mainPosts]);
 
-	useEffect(() => {
-  	if(retweetError) {
-  		alert(retweetError)
-  	}
+  useEffect(() => {
+    if (retweetError) {
+      alert(retweetError)
+    }
   }, [retweetError])
 
-	return (
-		<AppLayout>
-			{me && <PostForm/>}
-			{mainPosts.map((c) => {
-				return (
-					<PostCard key={c.id} post={c}/>
-				);
-			})}
-		</AppLayout>
-	);
+  return (
+    <AppLayout>
+      {me && <PostForm/>}
+      {mainPosts.map((c) => {
+        return (
+          <PostCard key={c.id} post={c}/>
+        );
+      })}
+    </AppLayout>
+  );
 };
+
+// 화면을 그리기 전에 서버쪽에서 먼저 실행
+// context 안에 store가 들어있다.
+export const getServerSideProps = wrapper.getServerSideProps((store) => async ({req, res, ...etc}) => {
+  store.dispatch({
+    type: LOAD_MY_INFO_REQUEST,
+  });
+
+  store.dispatch({
+    type: LOAD_POSTS_REQUEST
+  })
+  store.dispatch(END);
+  await store.sagaTask.toPromise();
+});
 
 export default Home;
--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -1,21 +1,24 @@
-import { HYDRATE } from 'next-redux-wrapper';
-import { combineReducers } from 'redux';
+import {HYDRATE} from 'next-redux-wrapper';
+import {combineReducers} from 'redux';
 
 import user from './user';
 import post from './post';
 
-const rootReducer = combineReducers({
-  index: (state = {}, action) => {
-    switch (action.type) {
-      case HYDRATE:
-        console.log('HYDRATE', action);
-        return { ...state, ...action.payload };
-      default:
-        return state;
+const rootReducer = (state, action) => {
+  // 루트 리듀서의 전체 상태를 덮어씌우기 위함
+  switch (action.type) {
+    case HYDRATE:
+      console.log('HYDRATE', action);
+      return action.payload;
+
+    default: {
+      const combineReducer = combineReducers({
+        user,
+        post,
+      });
+      return combineReducer(state, action);
     }
-  },
-  user,
-  post,
-});
+  }
+}
 
 export default rootReducer;
