88fe3810a7b2e6b48b1d90224d473c05a1f7fd62
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2022-01-16 13:56:06 +0900

    Feat: 해시태그 검색기능 구현

--- front/src/layout/AppLayout.js
+++ front/src/layout/AppLayout.js
@@ -1,29 +1,41 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {useSelector} from "react-redux";
 import PropTypes from 'prop-types';
+import Router from 'next/router';
 import Link from 'next/link';
 import styled from "styled-components";
 import {Menu, Input, Row, Col} from 'antd';
 
 import UserProfile from '../components/UserProfile';
 import LoginForm from '../components/LoginForm';
+import useInput from '../hooks/useInput';
 
 const AppLayout = ({children}) => {
+	const [searchInput, onChangeSearchInput] = useInput('');
 	const {me} = useSelector(state => state.user);
 
+	const onSearch = useCallback(() => {
+		Router.push(`/hashtag/${searchInput}`);
+	}, [searchInput]);
+	
 	return (
 		<div>
 			<Menu mode="horizontal">
-				<Menu.Item key="0">
+				<Menu.Item>
 					<Link href="/"><a>노드버드</a></Link>
 				</Menu.Item>
-				<Menu.Item key="1">
+				<Menu.Item>
 					<Link href="/profile"><a>프로필</a></Link>
 				</Menu.Item>
-				<Menu.Item key="2">
-					<SearchInput enterButton/>
+				<Menu.Item>
+					<SearchInput
+						enterButton
+						value={searchInput}
+						onChange={onChangeSearchInput}
+						onSearch={onSearch}
+					/>
 				</Menu.Item>
-				<Menu.Item key="3">
+				<Menu.Item>
 					<Link href="/signup"><a>회원가입</a></Link>
 				</Menu.Item>
 			</Menu>
